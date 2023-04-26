df4c91d7584b8263d9bf1e93fe04228b5787b722
Dadatata-JZ <jz290@login02.o2.rc.hms.harvard.edu>
2023-04-26 10:37:44 -0400

    Revision updates

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -39,6 +39,47 @@ from torchvision import datasets, models, transforms
 from PIL import Image
 from matplotlib import pylab as plt
 
+def CB_loss(labels, logits, samples_per_cls, no_of_classes, loss_type, beta, gamma, device = False):
+    """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
+    Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
+    where Loss is one of the standard losses used for Neural Networks.
+    Args:
+      labels: A int tensor of size [batch].
+      logits: A float tensor of size [batch, no_of_classes].
+      samples_per_cls: A python list of size [no_of_classes].
+      no_of_classes: total number of classes. int
+      loss_type: string. One of "sigmoid", "focal", "softmax".
+      beta: float. Hyperparameter for Class balanced loss.
+      gamma: float. Hyperparameter for Focal loss.
+    Returns:
+      cb_loss: A float tensor representing class balanced loss
+    """
+    if 0 in samples_per_cls: # some classes have 0 samples in this batch
+        samples_per_cls = [x if x != 0 else 1 for x in samples_per_cls]
+
+    effective_num = 1.0 - np.power(beta, samples_per_cls)
+    weights = (1.0 - beta) / np.array(effective_num)
+    weights = weights / np.sum(weights) * no_of_classes
+
+    labels_one_hot = F.one_hot(labels, no_of_classes).float()
+
+    weights = torch.tensor(weights).float().to(device)
+    weights = weights.unsqueeze(0)
+    weights = weights.repeat(labels_one_hot.shape[0], 1) * labels_one_hot
+    weights = weights.sum(1)
+    weights = weights.unsqueeze(1)
+    weights = weights.repeat(1, no_of_classes)
+
+    if loss_type == "focal":
+        cb_loss = focal_loss(labels_one_hot, logits, weights, gamma)
+    elif loss_type == "sigmoid":
+        cb_loss = F.binary_cross_entropy_with_logits(input=logits, target=labels_one_hot, weight=weights)
+    elif loss_type == "softmax":
+        pred = logits.softmax(dim=1)
+        cb_loss = F.binary_cross_entropy(input=pred, target=labels_one_hot, weight=weights)
+    return cb_loss
+
+
 class Meta_Dataset:
     def __init__(self, task='CancerVsBenign',db = 'BWH',
                  all_path='', seed=0,
